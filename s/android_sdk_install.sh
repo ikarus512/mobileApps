b87d0a27ac6ceb6ad345965ed401c334afeaccd2
ikarus512 <ikarus512@yandex.ru>
2017-09-17 20:50:14 +0300

    cosmetic

---
+++ s/android_sdk_install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export ANDROID_HOME=$PWD/android-sdk-linux
+export ANDROID_SDK=$ANDROID_HOME
+export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/23.0.2:${PATH}
+
+if [ ! -e android-sdk-linux/tools/bin ];then
+
+    wget --quiet http://dl.google.com/android/android-sdk_r24.4-linux.tgz
+    tar -xf android-sdk_r24.4-linux.tgz
+
+    #android list sdk --extended # && android list sdk -a --extended
+
+    ANDROPACKS=tools,platform-tools,build-tools-26.0.1,android-16,android-26
+    ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk -u -a -f -t ${ANDROPACKS}    >/dev/null 2>&1
+
+    ### Accept licenses:
+    # - mkdir $ANDROID_HOME/licenses
+    # - echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
+    # - echo -e "\n84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_HOME/licenses/android-sdk-preview-license"
+
+    # That requests us to accept a license for the sdkmanager, and then
+    echo y | sdkmanager --update    >/dev/null 2>&1
+    # requests us to accept new licenses not previously accepted
+    ( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | sdkmanager --licenses    >/dev/null 2>&1
+
+fi
