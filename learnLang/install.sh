fa94b354f9b51be31e61b50e48d08dbc6b2a093d
ikarus512 <ikarus512@yandex.ru>
2017-09-17 21:54:44 +0300

    cosmetic

--- learnLang/install.sh
+++ learnLang/install.sh
@@ -6,9 +6,16 @@ chmod -R +x hooks
 
 if [ ! -e platforms/android ];then
 
-    cordova prepare || exit 1
+    # cordova prepare || exit 1
     # cordova prepare --verbose || exit 1
 
+    # cordova platform add android #--save
     cordova platform add android || exit 1
+
+    #cordova plugin add cordova-plugin-dialogs      ## navigator.notification.alert()
+    cordova plugin add cordova-plugin-crosswalk-webview
+
+    echo Android app permissions (platforms/android/AndroidManifest.xml):
+    cat platforms/android/AndroidManifest.xml | grep permission
 
 fi
