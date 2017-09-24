0b8803c6942c8a5d1e232590d81546421cd08160
ikarus512 <ikarus512@yandex.ru>
2017-09-25 00:05:27 +0300

    cosmetic

--- apps/learnLang/scripts/check-permissions.sh
+++ apps/learnLang/scripts/check-permissions.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-ls platforms
-ls plugins
+echo "=== platforms:"
+ls $APP_DIR/platforms
+echo "=== plugins:"
+ls $APP_DIR/plugins
+echo "==="
 
-mv -fv $ANDROID_MANIFEST $ANDROID_MANIFEST.old
-echo "Android app permissions old ($ANDROID_MANIFEST):"
-grep    permission $ANDROID_MANIFEST.old || echo "    (no matches found)"
-grep -v permission $ANDROID_MANIFEST.old >$ANDROID_MANIFEST
-echo "Android app permissions new ($ANDROID_MANIFEST):"
+# mv -fv $ANDROID_MANIFEST $ANDROID_MANIFEST.old
+# echo "Android app permissions old ($ANDROID_MANIFEST):"
+# grep    permission $ANDROID_MANIFEST.old || echo "    (no matches found)"
+# grep -v permission $ANDROID_MANIFEST.old >$ANDROID_MANIFEST
+# echo "Android app permissions new ($ANDROID_MANIFEST):"
+# grep    permission $ANDROID_MANIFEST || echo "    (no matches found)"
+
+echo "Android app permissions ($ANDROID_MANIFEST):"
 grep    permission $ANDROID_MANIFEST || echo "    (no matches found)"
