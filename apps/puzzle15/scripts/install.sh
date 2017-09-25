942406df632d1fc96cc149b857f8b3347867d93b
ikarus512 <ikarus512@yandex.ru>
2017-09-25 13:58:55 +0300

    puzzle15 fix

--- apps/puzzle15/scripts/install.sh
+++ apps/puzzle15/scripts/install.sh
@@ -6,25 +6,24 @@ mkdir -p hooks
 chmod -R +x hooks
 ls -hl hooks
 
-if [ ! -e platforms/android -o ! -e plugins ];then
+if [ ! -e platforms/android ];then
 
     # cordova prepare || exit 1
     # cordova prepare --verbose || exit 1
 
     # cordova platform add android #--save
     cordova platform add android || exit 1
-
-    cordova plugin add cordova-plugin-dialogs
-    cordova plugin add cordova-plugin-media
-
-    if [ "$OPT1" == "debug" ];then
-        ### remove debug plugins
-        # cordova plugin add cordova-plugin-console
-        # cordova plugin add cordova-plugin-dialogs ### navigator.notification.alert()
-        cordova plugin add cordova-plugin-crosswalk-webview
-    fi
-
 fi
+
+if [ ! -e plugins/cordova-plugin-dialogs ];then cordova plugin add cordova-plugin-dialogs; fi
+if [ ! -e plugins/cordova-plugin-media ];then cordova plugin add cordova-plugin-media; fi ### navigator.notification.alert()
+
+if [ ! -e plugins/cordova-plugin-crosswalk-webview -a "$OPT1" == "debug" ];then
+    cordova plugin add cordova-plugin-crosswalk-webview
+fi
+
+### remove debug plugins
+# cordova plugin add cordova-plugin-console
 
 echo "=== android check permissions:"
 source $HOME_DIR/scripts/android_check_permissions.sh
