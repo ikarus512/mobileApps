14381c13bb6e857cadd0ac5a8068fd0e9aaa9643
ikarus512 <ikarus512@yandex.ru>
2017-10-27 00:24:35 +0300

    nw.js: first try to build for Windows/Linux/MacOS

--- apps/learnLang/scripts/before_install.sh
+++ apps/learnLang/scripts/before_install.sh
@@ -2,8 +2,8 @@
 
 case $PLAT in
 android) pushd $HOME_DIR; source $HOME_DIR/scripts/android_sdk_install.sh; popd ;;
-#win32) sudo apt-get install wine;;
-#nwjs) sudo apt-get install wine;;
+#win32) sudo apt-get install wine nsis;;
+#nwjs) sudo apt-get install wine nsis;;
 esac
 
 case $APP in
