19b4cd67a1da8e0f215785dc24aafb2a595e8d92
ikarus512 <ikarus512@yandex.ru>
2017-11-05 12:14:20 +0300

    cosmetic

--- apps/learnLang/scripts/build.sh
+++ apps/learnLang/scripts/build.sh
@@ -28,15 +28,17 @@ case $TRG_OS in
 linux-win)
     ### Create installations using nw.js distro and www folder (www compiled from src)
     mydo pushd scripts
-    wget --quiet https://dl.nwjs.io/v0.26.2/nwjs-v0.26.2-linux-ia32.tar.gz &
-    wget --quiet https://dl.nwjs.io/v0.26.2/nwjs-v0.26.2-linux-x64.tar.gz &
-    wget --quiet https://dl.nwjs.io/v0.26.2/nwjs-v0.26.2-win-ia32.zip &
-    wget --quiet https://dl.nwjs.io/v0.26.2/nwjs-v0.26.2-win-x64.zip &
-    mydo wait %1 %2 %3 %4
-    . ./create-installer-win-ia32-nsis-nwjs.sh
-    . ./create-installer-win-x64-nsis-nwjs.sh
-    . ./create-installer-linux-ia32-nsis-nwjs.sh
-    . ./create-installer-linux-x64-nsis-nwjs.sh
+        echo "### wget started"
+        wget --quiet https://dl.nwjs.io/v0.26.2/nwjs-v0.26.2-linux-ia32.tar.gz &
+        wget --quiet https://dl.nwjs.io/v0.26.2/nwjs-v0.26.2-linux-x64.tar.gz &
+        wget --quiet https://dl.nwjs.io/v0.26.2/nwjs-v0.26.2-win-ia32.zip &
+        wget --quiet https://dl.nwjs.io/v0.26.2/nwjs-v0.26.2-win-x64.zip &
+        wait %1 %2 %3 %4
+        echo "### wget finished"
+        . ./create-installer-win-ia32-nsis-nwjs.sh
+        . ./create-installer-win-x64-nsis-nwjs.sh
+        . ./create-installer-linux-ia32-nsis-nwjs.sh
+        . ./create-installer-linux-x64-nsis-nwjs.sh
     mydo popd
 
     mydo ls -l $RELEASES_DIR
