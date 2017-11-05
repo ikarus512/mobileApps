f17cd39e62e50c23d33815036f872d5dd25c3877
ikarus512 <ikarus512@yandex.ru>
2017-11-06 00:49:05 +0300

    osx: deploy github release fix

--- scripts/_mydo.sh
+++ scripts/_mydo.sh
@@ -34,34 +34,32 @@
 #
 #*/
 mydo () {
-  local head_n=$(echo $*|perl -e '$_=<>; if(m/^.*--mydo-head-(\d*).*$/) { print $1; }')
-  local tail_n=$(echo $*|perl -e '$_=<>; if(m/^.*--mydo-tail-(\d*).*$/) { print $1; }')
-  local cmd=$(echo $*|perl -e '$_=<>; s/--mydo-(tail|head)-\d*//g; print;')
-  local head_then_tail=$(echo $*|perl -e '$_=<>; if(m/^.*--mydo-head-(\d*).*--mydo-tail-(\d*).*$/) { print $1; }')
+  local acmd=$*
+  local head_n=$(echo $acmd|perl -e '$_=<>; if(m/^.*--mydo-head-(\d+).*$/) { print $1; }')
+  local tail_n=$(echo $acmd|perl -e '$_=<>; if(m/^.*--mydo-tail-(\d+).*$/) { print $1; }')
+  local cmd=$(echo $acmd|perl -e '$_=<>; s/--mydo-(head|tail)-\d+//g; print;')
+  local head_then_tail=$(echo $acmd|perl -e '$_=<>; if(m/^.*--mydo-head-(\d+).*--mydo-tail-(\d+).*$/) { print $1; }')
   local exitcode
   if [ "$tail_n" == "" -a "$head_n" == "" ];then
     echo "### $cmd"
     $cmd
     return $?
-  else if [ "$head_n" == "" ];then
+  elif [ "$head_n" == "" ];then
     echo "### $cmd | tail -n $tail_n"
    #$cmd | tail -n $tail_n                                      # It does not catch STDERR
     $cmd >mydo.tmp.log 2>&1; exitcode=$?; cat mydo.tmp.log | tail -n $tail_n  # It catches STDERR
     return $exitcode
-  else if [ "$tail_n" == "" ];then
+  elif [ "$tail_n" == "" ];then
     echo "### $cmd | head -n $head_n"
-   #$cmd | tail -n $head_n                                      # It does not catch STDERR
-    $cmd >mydo.tmp.log 2>&1; exitcode=$?; cat mydo.tmp.log | tail -n $head_n  # It catches STDERR
+    $cmd >mydo.tmp.log 2>&1; exitcode=$?; cat mydo.tmp.log | head -n $head_n
     return $exitcode
-  else if [ "$head_then_tail" != "" ];then
+  elif [ "$head_then_tail" != "" ];then
     echo "### $cmd | head -n $head_n | tail -n $tail_n"
-   #$cmd | tail -n $head_n                                      # It does not catch STDERR
-    $cmd >mydo.tmp.log 2>&1; exitcode=$?; cat mydo.tmp.log | head -n $head_n | tail -n $tail_n  # It catches STDERR
+    $cmd >mydo.tmp.log 2>&1; exitcode=$?; cat mydo.tmp.log | head -n $head_n | tail -n $tail_n
     return $exitcode
-  else #if [ "$head_then_tail" == "" ];then
+  else #elif [ "$head_then_tail" == "" ];then
     echo "### $cmd | tail -n $tail_n | head -n $head_n"
-   #$cmd | tail -n $head_n                                      # It does not catch STDERR
-    $cmd >mydo.tmp.log 2>&1; exitcode=$?; cat mydo.tmp.log | tail -n $tail_n | head -n $head_n  # It catches STDERR
+    $cmd >mydo.tmp.log 2>&1; exitcode=$?; cat mydo.tmp.log | tail -n $tail_n | head -n $head_n
     return $exitcode
   fi
 }
