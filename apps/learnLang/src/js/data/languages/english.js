936bfdf681b3640dfd896bfd162e3b392193454c
ikarus512 <ikarus512@yandex.ru>
2018-03-22 01:11:52 +0300

    english added

---
+++ apps/learnLang/src/js/data/languages/english.js
@@ -0,0 +1,27 @@
+"use strict";
+var a = a || {};
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/english/01 adjectives",
+    ru: "языки    /английский/01 прилагательные",
+    terms: [
+        {term:"amused/amusing", translation:{en:"ие", ru:"веселый,довольный/забавный"}},
+        {term:"awkwardly/awkward", translation:{en:"translation2", ru:"неуклюже/неловкий,неудобный"}},
+        {term:"grumpy", translation:{en:"translation3", ru:"сварливый,раздражительный"}},
+        {term:"insecure", translation:{en:"translation4", ru:"неуверенный,ненадежный,небезопасный"}},
+        {term:"unsociable,unsocial", translation:{en:"translation4", ru:"необщительный"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/arabian العربية /common phrases",
+    ru: "языки    /арабский العربية/общие фразы   ",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
