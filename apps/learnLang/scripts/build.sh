7b8833fb287f22c6649b3098e403b2f5d3b48b12
evgeny <evgeny@evgenys-iMac.local>
2018-10-25 22:43:53 +0300

    ios platform fix

--- apps/learnLang/scripts/build.sh
+++ apps/learnLang/scripts/build.sh
@@ -59,6 +59,7 @@ ios)
     #mydo cordova build ios --device --debug --verbose || exit 1
     #sed -i "" "s/iPhone Distribution/iPhone Developer/g" $APPL_DIR/platforms/ios/cordova/build-release.xcconfig
     #mydo cordova build ios --device --release  --codeSignIdentity=$IOS_DEVELOPER_NAME
+        rvm use system
         echo "### cordova build ios"; cordova build ios --device --release  --codeSignIdentity="iPhone Developer" --developmentTeam=CU6FE4TYC9 --provisioningProfile=a2b92060-29ef-4dc6-96ee-424d45060223 --packageType=development #--automaticProvisioning=true
     #mydo cordova build ios --device --release #|| exit 1
     #echo "### ls $HOME/Library/MobileDevice/Provisioning\ Profiles"; ls $HOME/Library/MobileDevice/Provisioning\ Profiles
