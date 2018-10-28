d309fe91d766ddf2c1750893e48b1fe9b7973c71
evgeny <evgeny@evgenys-iMac.local>
2018-10-28 19:29:07 +0300

    ios platform fix

--- apps/learnLang/scripts/ios-certs-profile/prepare.sh
+++ apps/learnLang/scripts/ios-certs-profile/prepare.sh
@@ -1,3 +1,4 @@
-openssl aes-256-cbc -k "$IOS_ENCRIPTION_SECRET" -in learnLang.mobileprovision -out learnLang.mobileprovision.enc -a
-openssl aes-256-cbc -k "$IOS_ENCRIPTION_SECRET" -in dist.cer -out dist.cer.enc -a
-openssl aes-256-cbc -k "$IOS_ENCRIPTION_SECRET" -in dist.p12 -out dist.p12.enc -a
+#https://www.objc.io/issues/6-build-tools/travis-ci
+openssl aes-256-cbc -k "$IOS_ENCRYPTION_SECRET" -in learnLang.mobileprovision -out learnLang.mobileprovision.enc -a
+openssl aes-256-cbc -k "$IOS_ENCRYPTION_SECRET" -in dist.cer -out dist.cer.enc -a
+openssl aes-256-cbc -k "$IOS_ENCRYPTION_SECRET" -in dist.p12 -out dist.p12.enc -a
