525dc2d1db14760d93da1e5f5052c92de4bc084f
ikarus512 <ikarus512@yandex.ru>
2020-08-29 22:50:42 +0300

    learnlang android chineeze

--- apps/learnLang/src/js/data/languages/chinese.js
+++ apps/learnLang/src/js/data/languages/chinese.js
@@ -3,8 +3,20 @@ var a = a || {};
 
 a.learnData.add({
     //"font-size": "1.1em",
-    en: "languages/chineeze 普通話中文 /alphabet",
-    ru: "языки    /китайский 普通話中文/алфавит ",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 1",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 1",
+    terms: [
+        {term:"pin yin", transcription:"[āáăàa īíĭìi ūúŭùu ǖǘǚǜü ōóŏòo ēéěèe]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 2",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 2",
     terms: [
         {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
         {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
@@ -15,8 +27,164 @@ a.learnData.add({
 
 a.learnData.add({
     //"font-size": "1.1em",
-    en: "languages/chineeze 普通話中文 /common phrases/1",
-    ru: "языки    /китайский 普通話中文/общие фразы   /1",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 3",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 3",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 4",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 4",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 5",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 5",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 6",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 6",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 7",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 7",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 8",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 8",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 9",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 9",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 10",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 10",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 11",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 11",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 12",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 12",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 13",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 13",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 14",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 14",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 15",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 15",
+    terms: [
+        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
+        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
+        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
+        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+    ],
+});
+
+a.learnData.add({
+    //"font-size": "1.1em",
+    en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 16",
+    ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 16",
     terms: [
         {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
         {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
