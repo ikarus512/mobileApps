ef04d3cb96ceb6ab78e29b8c62a9348c1fe89f74
ikarus512 <ikarus512@yandex.ru>
2020-08-31 23:03:13 +0300

    chineeze lesson 2

--- apps/learnLang/src/js/data/languages/chinese.js
+++ apps/learnLang/src/js/data/languages/chinese.js
@@ -26,25 +26,18 @@ a.learnData.add({
         {term:"ma?", transcription:"[ма?]", translation:{en:"wether?", ru:"ли?"}},
         {term:"nĭ hăo ma?", transcription:"[ни хао ма?]", translation:{en:"How are you?", ru:"Как дела?"}},
 
-        {term:"hěn", transcription:"[]", translation:{en:"very", ru:"очень"}},
-        {term:"wŏ hěn hăo", transcription:"[]", translation:{en:"I'm (very) fine", ru:"У меня все (очень) хорошо"}},
-        {term:"nĭ ne?", transcription:"[]", translation:{en:"And you?", ru:"А ты? (А у тебя?)"}},
+        {term:"hěn", transcription:"", translation:{en:"very", ru:"очень"}},
+        {term:"wŏ hěn hăo", transcription:"", translation:{en:"I'm (very) fine", ru:"У меня все (очень) хорошо"}},
+        {term:"nĭ ne?", transcription:"", translation:{en:"And you?", ru:"А ты? (А у тебя?)"}},
 
         {term:"xiè xie", transcription:"[се се]", translation:{en:"Thank you", ru:"Спасибо"}},
-        {term:"xiè xie nĭ men", transcription:"[]", translation:{en:"Thank you (to you plural)", ru:"Спасибо вам"}},
+        {term:"xiè xie nĭ men", transcription:"", translation:{en:"Thank you (to you plural)", ru:"Спасибо вам"}},
 
         {term:"wŏ de", transcription:"[уо дэ]", translation:{en:"my", ru:"мой"}},
         {term:"nĭ de", transcription:"[ни дэ]", translation:{en:"your", ru:"твой"}},
         {term:"wŏ men de", transcription:"[уо мэн дэ]", translation:{en:"our", ru:"наш"}},
 
         {term:"zài jiàn", transcription:"[дзай дзянь]", translation:{en:"Good bue", ru:"Досвидания"}},
-
-        // āáăàa
-        // īíĭìi
-        // ūúŭùu ǖǘǚǜü
-        // ōóŏòo
-        // ēéěèe
-//        {term:"tā", transcription:"[та]", translation:{en:"", ru:""}},
     ],
 });
 
@@ -53,10 +46,46 @@ a.learnData.add({
     en: "languages/chineeze 普通話中文 /tvkultura.ru-poliglot/lesson 2",
     ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 2",
     terms: [
-        {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
-        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
-        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
-        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+        {term:"yě", transcription:"", translation:{en:"too, either", ru:"тоже"}},
+        {term:"wŏ yě hěn hăo", transcription:"", translation:{en:"I'm (very) fine too", ru:"У меня тоже все (очень) хорошо"}},
+        {term:"shén me ?..", transcription:"", translation:{en:"what ?..", ru:"что, какой ?.."}},
+        {term:"shéi?", transcription:"", translation:{en:"who?", ru:"кто?"}},
+        {term:"năr?", transcription:"[нар]", translation:{en:"where?", ru:"где, куда?"}},
+        {term:"shì", transcription:"", translation:{en:"to be", ru:"быть, являться"}},
+        {term:"tā shì shéi?", transcription:"", translation:{en:"who is he", ru:"кто он (есть)?"}},
+        {term:"jiào", transcription:"[дзяо]", translation:{en:"named", ru:"звать, называться"}},
+        {term:"míng zi", transcription:"[минг дзы]", translation:{en:"name", ru:"имя"}},
+        {term:"nĭ jiào shén me míng zi", transcription:"", translation:{en:"what is your name?", ru:"как тебя зовут"}},
+        {term:"wŏ jiào", transcription:"", translation:{en:"my name is ...", ru:"меня зовут ..."}},
+        {term:"zhù", transcription:"[джу]", translation:{en:"live", ru:"жить"}},
+        {term:"zài", transcription:"[дзай]", translation:{en:"in", ru:"в"}},
+        {term:"wŏ zhù zài mò sī kē", transcription:"", translation:{en:"I live in Moskow", ru:"Я живу в Москве"}},
+
+        {term:"è", transcription:"[ыэ]", translation:{en:"russian", ru:"русский"}},
+        {term:"è luó sī", transcription:"[ыэ луо сы]", translation:{en:"Russia", ru:"Россия"}},
+        {term:"gé lŭ jì yà", transcription:"", translation:{en:"Georgia (Cartvelia)", ru:"Грузия"}},
+        {term:"rén", transcription:"[жень, рэнь]", translation:{en:"a person", ru:"человек"}},
+        {term:"zhōng guó rén", transcription:"", translation:{en:"a chineeze", ru:"китаец"}},
+        {term:"zhōng guó", transcription:"", translation:{en:"middle state", ru:"серединная страна"}},
+        {term:"wŏ shì mò sī kē rén", transcription:"", translation:{en:"I'm a Moskow person", ru:"Я москвич(-ка)"}},
+        {term:"nĭ zhù zài năr?", transcription:"", translation:{en:"Where do you live?", ru:"Где ты живешь?"}},
+        {term:"nă?", transcription:"[на]", translation:{en:"what?", ru:"какой?"}},
+        {term:"nĭ shì nă guó rén?", transcription:"", translation:{en:"What country are you from?", ru:"Ты из какой страны?"}},
+
+        {term:"gē shŏu", transcription:"[гэ шоу]", translation:{en:"singer", ru:"певец"}},
+        {term:"wŏ shì gē shŏu", transcription:"", translation:{en:"I am a singer", ru:"Я (есть) певец"}},
+        {term:"yăn yuán", transcription:"[ян йюэн]", translation:{en:"an actor", ru:"актер"}},
+        {term:"zhŭ chí rén", transcription:"", translation:{en:"TV presenter (anchorman)", ru:"(теле-) ведущий"}},
+        {term:"zhì piān rén", transcription:"", translation:{en:"producer", ru:"продюсер"}},
+        {term:"shè yĭng shī", transcription:"", translation:{en:"photographer", ru:"фотограф"}},
+        {term:"dăo yuán", transcription:"", translation:{en:"producer (director)", ru:"режиссер"}},
+
+        {term:"dăo yuán", transcription:"", translation:{en:"producer (director)", ru:"режиссер"}},
+        {term:"gōng zuò", transcription:"[гон дзуо]", translation:{en:"to work, a work", ru:"работа(-ть)"}},
+        {term:"zuò", transcription:"", translation:{en:"do", ru:"делать"}},
+
+        {term:"nĭ zuò shén me gōng zuò?", transcription:"", translation:{en:"who do you work as?", ru:"кем ты работаешь (делаешь какую работу)?"}},
+        {term:"lăo shī", transcription:"", translation:{en:"a teacher", ru:"учитель"}},
     ],
 });
 
@@ -66,9 +95,11 @@ a.learnData.add({
     ru: "языки    /китайский 普通話中文/tvkultura.ru-полиглот/урок 3",
     terms: [
         {term:"term1", transcription:"[term1]", translation:{en:"translation1", ru:"перевод1"}},
-        {term:"term2", transcription:"[term2]", translation:{en:"translation2", ru:"перевод2"}},
-        {term:"term3", transcription:"[term3]", translation:{en:"translation3", ru:"перевод3"}},
-        {term:"term4", transcription:"[term4]", translation:{en:"translation4", ru:"перевод4"}},
+        // āáăàa
+        // īíĭìi
+        // ūúŭùu ǖǘǚǜü
+        // ōóŏòo
+        // ēéěèe
     ],
 });
 
